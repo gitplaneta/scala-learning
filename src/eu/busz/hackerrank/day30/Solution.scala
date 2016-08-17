89ed5b4f0e72a716b70d57eb24367ce408e581c8
Radoslaw Busz <radoslaw.busz@yoti.com>
2016-08-17 17:07:54 +0100

    hackerrank 30

---
+++ src/eu/busz/hackerrank/day30/Solution.scala
@@ -0,0 +1,25 @@
+package eu.busz.hackerrank.day30
+
+object Solution {
+
+  def main(args: Array[String]) {
+    val pattern = ("[a-z\\.]+@gmail\\.com")
+    val sc = new java.util.Scanner (System.in)
+    val N = sc.nextInt()
+    var list = List[(String, String)]()
+    var a0 = 0
+    while(a0 < N){
+      var firstName = sc.next()
+      var emailID = sc.next()
+      a0+=1
+      list = (firstName, emailID) :: list
+    }
+
+    list
+      .filter{case (name, email) => email.matches(pattern)}
+      .map{case (name, email) => name}
+      .sortWith(_ < _)
+      .foreach(println(_))
+  }
+
+}
